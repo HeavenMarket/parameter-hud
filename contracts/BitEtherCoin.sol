2d62d504c1151176afb6487acc9d0ca4d3a40109
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2020-04-12 20:15:57 +0000

    Remove var_dump

--- contracts/BitEtherCoin.sol
+++ contracts/BitEtherCoin.sol
@@ -20,3 +20,24 @@ contract BitEtherCoin {
 
     // Initial reward per Ethereum Block is 2,
     // for 25 block it gives same reward as for Bitcoin first era (2 * 25 == 50)
+    //
+    //                   2.00000000
+    uint256 rewardBase = 200000000;
+
+    uint256  eraSize =    5250000;
+    uint256  startBlock = 2726892;
+
+    function BitEtherCoin() {
+        satoshi = msg.sender;
+    }
+
+    // Mining Operations
+
+    function claim() returns (uint256) {
+        var (eraId, eraBlock, reward, prevReward) = getEra();
+        if (minedBlock >= block.number) {
+            Reward(block.coinbase, reward, false);
+        } else if (eraId > 0 && eraId < 30) {
+            uint256 unclaimed = getUnclaimed(eraBlock, minedBlock, block.number, prevReward, reward);
+
+            if (reward > 0) {
