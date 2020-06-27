df0a6bd531044f81b6c4e5275ef3270acf763d35
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2020-06-27 12:34:34 +0000

    Undo last change

--- test/eras.js
+++ test/eras.js
@@ -3,4 +3,4 @@ contract('Calculate Era', function(accounts) {
     var addrClient = accounts[1];
 
     it("block        1 - not started - 0", function(done) {
-       
+        var coin = BitEtherCoin.deployed();
