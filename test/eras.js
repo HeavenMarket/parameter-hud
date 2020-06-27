e5f14a83035a9f87d2a072cfd916ab3c4deb8de5
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2020-06-27 10:54:36 +0000

    update translation file

--- test/eras.js
+++ test/eras.js
@@ -2,4 +2,5 @@ contract('Calculate Era', function(accounts) {
 
     var addrClient = accounts[1];
 
-    it("block        1 - not starte
+    it("block        1 - not started - 0", function(done) {
+       
