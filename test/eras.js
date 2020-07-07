7c1f5eaff63a7d5b35d65a9dc9720fef6c2c3fda
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2020-07-07 14:24:55 +0000

    add editorconfig

--- test/eras.js
+++ test/eras.js
@@ -6,4 +6,5 @@ contract('Calculate Era', function(accounts) {
         var coin = BitEtherCoin.deployed();
         coin.getEraForBlock.call(1).then((e) => {
             assert.equal(e[0].toNumber(), 0);
-            assert.equal(e[1].toNumber(), 0)
+            assert.equal(e[1].toNumber(), 0);
+            asser
