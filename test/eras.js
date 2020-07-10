212c5439e4e668e5451e5e0712f3f3c6f01eca1d
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2020-07-10 21:37:08 +0000

    new bundle

--- test/eras.js
+++ test/eras.js
@@ -9,4 +9,5 @@ contract('Calculate Era', function(accounts) {
             assert.equal(e[1].toNumber(), 0);
             assert.equal(e[2].toNumber(), 0);
             assert.equal(e[3].toNumber(), 0);
- 
+        }).then(done).catch(done);
+  
