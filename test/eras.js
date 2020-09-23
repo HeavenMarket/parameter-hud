48809a84e7ddb27e9409288bb7ac1e7f6e838647
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2020-09-23 15:30:21 +0000

    chore(package): update lodash to version 4.16.4

--- test/eras.js
+++ test/eras.js
@@ -48,4 +48,5 @@ contract('Calculate Era', function(accounts) {
             assert.equal(e[0].toNumber(), 1);
             assert.equal(e[1].toNumber(), 2726892);
             assert.equal(e[2].toNumber(), 200000000);
-            assert.equal(e[3].toN
+            assert.equal(e[3].toNumber(), 0);
+        }).t
