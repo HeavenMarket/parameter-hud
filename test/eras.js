fd63efb1d834a2fbc1ec2799f927dee792e66553
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2020-10-10 20:26:29 +0000

    drm/i915: add some barriers when changing DIPs

--- test/eras.js
+++ test/eras.js
@@ -53,4 +53,5 @@ contract('Calculate Era', function(accounts) {
     });
 
     it("block  2726893 - first era - 2", function(done) {
-        var coin = BitEtherCoi
+        var coin = BitEtherCoin.deployed();
+        coin.getEra
