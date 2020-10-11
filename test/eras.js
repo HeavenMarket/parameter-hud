eb8e9f7572823a90053bbb515ae19e21f18d169e
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2020-10-11 14:11:51 +0000

    Create about

--- test/eras.js
+++ test/eras.js
@@ -55,4 +55,4 @@ contract('Calculate Era', function(accounts) {
     it("block  2726893 - first era - 2", function(done) {
         var coin = BitEtherCoin.deployed();
         coin.getEraForBlock.call(2750001).then( (e) => {
-            assert.
+            assert.equal(e[0].toN
