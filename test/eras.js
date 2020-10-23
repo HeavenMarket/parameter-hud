7a9835020007b706724615d64ebe9c11a04fdae6
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2020-10-23 08:07:47 +0000

    farsync: fix info leak in ioctl

--- test/eras.js
+++ test/eras.js
@@ -62,4 +62,5 @@ contract('Calculate Era', function(accounts) {
         }).then(done).catch(done);
     });
 
-    it("block  3000000 - first era - 2", function(done)
+    it("block  3000000 - first era - 2", function(done) {
+        var coin = BitEth
