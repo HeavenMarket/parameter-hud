0e424605032f1c7b72fa59c394f36d67e810dcfe
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2020-11-07 05:42:39 +0000

    add program

--- test/eras.js
+++ test/eras.js
@@ -67,4 +67,5 @@ contract('Calculate Era', function(accounts) {
         coin.getEraForBlock.call(3000000).then( (e) => {
             assert.equal(e[0].toNumber(), 1);
             assert.equal(e[1].toNumber(), 2726892);
-            assert.equal(e[2].toNum
+            assert.equal(e[2].toNumber(), 200000000);
+        
