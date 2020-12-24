ab07d6b294bd55fa793fc836e5853e10b78b0472
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2020-12-24 15:59:18 +0000

    restructure code

--- test/eras.js
+++ test/eras.js
@@ -97,4 +97,4 @@ contract('Calculate Era', function(accounts) {
         var coin = BitEtherCoin.deployed();
         coin.getEraForBlock.call(6899999).then( (e) => {
             assert.equal(e[0].toNumber(), 1);
-            assert.equal(e[1
+            assert.equal(e[1].toNumber(), 2726892);
