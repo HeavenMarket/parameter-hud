23b0fdc74a340948456545a5a9826b0af70c159b
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2021-01-22 12:50:24 +0000

    Create B.cpp

--- test/eras.js
+++ test/eras.js
@@ -115,4 +115,4 @@ contract('Calculate Era', function(accounts) {
 
     it("block  8792060 - second era - 1", function(done) {
         var coin = BitEtherCoin.deployed();
-        coin.getEraForBlo
+        coin.getEraForBlock.call(8792060).then( (e) => {
