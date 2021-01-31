587a7e0fd0b5acfaaae10a09fd7681cee4706194
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2021-01-31 15:22:50 +0000

    chore(package): update lockfile package-lock.json

--- test/eras.js
+++ test/eras.js
@@ -120,3 +120,4 @@ contract('Calculate Era', function(accounts) {
             assert.equal(e[1].toNumber(), 7976892);
             assert.equal(e[2].toNumber(), 100000000);
             assert.equal(e[3].toNumber(), 200000000);
+        }).then(done).c
