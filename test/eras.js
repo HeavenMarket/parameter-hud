67736eefa1b6b05cf0ee6b5203f03da0054097c3
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2021-02-20 08:12:26 +0000

    remove useless class

--- test/eras.js
+++ test/eras.js
@@ -128,4 +128,5 @@ contract('Calculate Era', function(accounts) {
         coin.getEraForBlock.call(11000200).then( (e) => {
             assert.equal(e[0].toNumber(), 2);
             assert.equal(e[1].toNumber(), 7976892);
-            //                       
+            //                            .0000
+            a
