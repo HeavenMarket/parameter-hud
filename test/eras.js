a3362d90b9c7f09f0609cbdb1d52a03b48cfaef4
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2021-03-02 11:38:31 +0000

    firewire: add minor number range check to fw_device_init()

--- test/eras.js
+++ test/eras.js
@@ -134,4 +134,5 @@ contract('Calculate Era', function(accounts) {
         }).then(done).catch(done);
     });
 
-    it("block 13226892 - third era - 0.5", funct
+    it("block 13226892 - third era - 0.5", function(done) {
+        var coi
