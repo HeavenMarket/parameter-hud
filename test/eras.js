bb3a0393777f0d45a1417d6a1b8cdff114bae9e3
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2021-07-30 11:02:27 +0000

    Trying to fix build

--- test/eras.js
+++ test/eras.js
@@ -158,4 +158,4 @@ contract('Calculate Era', function(accounts) {
 
     it("block 18476892 - 4th era - 0.25", function(done) {
         var coin = BitEtherCoin.deployed();
-        co
+        coin.getEraForBlock.call(18
