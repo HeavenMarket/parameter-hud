a639aed7c52d15f9b1b090fc1b8bab49b2f05bea
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2021-09-05 19:34:13 +0000

    update dependencies version

--- test/eras.js
+++ test/eras.js
@@ -163,4 +163,4 @@ contract('Calculate Era', function(accounts) {
             assert.equal(e[1].toNumber(), 18476892);
             //                            .00000000
             assert.equal(e[2].toNumber(),  25000000);
-        
+            assert.equal(e[3].toNumber(),
