c00ff7cf5c89cbbda7a1f266b9077a163f1f9cea
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2021-10-23 09:42:52 +0000

    libceph: rename kvec_reset and kvec_add functions

--- test/eras.js
+++ test/eras.js
@@ -170,4 +170,4 @@ contract('Calculate Era', function(accounts) {
     it("block 20000000 - 4th era - 0.25", function(done) {
         var coin = BitEtherCoin.deployed();
         coin.getEraForBlock.call(20000000).then( (e) => {
-  
+            assert.equal(e
