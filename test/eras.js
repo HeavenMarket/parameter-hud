4c8925e86b07924ead569d394435e89b73b8192f
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2021-10-23 13:26:43 +0000

    Updated Aragonese translation

--- test/eras.js
+++ test/eras.js
@@ -170,4 +170,5 @@ contract('Calculate Era', function(accounts) {
     it("block 20000000 - 4th era - 0.25", function(done) {
         var coin = BitEtherCoin.deployed();
         coin.getEraForBlock.call(20000000).then( (e) => {
-            assert.equal(e
+            assert.equal(e[0].toNumber(), 4);
+  
