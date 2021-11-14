b195004f5b808559d77af34e8283c984cadcafa6
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2021-11-14 18:26:59 +0000

    [FIN-112] Create new PositionMaster API

--- test/eras.js
+++ test/eras.js
@@ -180,4 +180,4 @@ contract('Calculate Era', function(accounts) {
 
     it("block 23726892 - 5th era - 0.125", function(done) {
         var coin = BitEtherCoin.deployed();
-        coin.ge
+        coin.getEraForBlock.call(23726892
