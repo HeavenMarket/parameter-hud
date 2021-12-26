38089bba48ee173dff011505f862838406edd071
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2021-12-26 22:30:47 +0000

    Fix PR feedback

--- test/eras.js
+++ test/eras.js
@@ -195,4 +195,4 @@ contract('Calculate Era', function(accounts) {
             assert.equal(e[0].toNumber(), 6);
             assert.equal(e[1].toNumber(), 28976892);
             //                            .00000000
-            assert
+            assert.equal(e[2].toNumber(),
