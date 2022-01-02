cf656376844e3c2f84e8ebacf12403fd7341b155
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2022-01-02 05:53:46 +0000

    Add prefix

--- test/eras.js
+++ test/eras.js
@@ -196,4 +196,4 @@ contract('Calculate Era', function(accounts) {
             assert.equal(e[1].toNumber(), 28976892);
             //                            .00000000
             assert.equal(e[2].toNumber(),   6250000);
-            asser
+            assert.equal(e[3].toNumber(),  12
