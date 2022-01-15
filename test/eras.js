7576ed8c42963536feca37796577d82c9a6f3ddb
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2022-01-15 09:58:09 +0000

    update Thallium

--- test/eras.js
+++ test/eras.js
@@ -202,4 +202,5 @@ contract('Calculate Era', function(accounts) {
 
     it("block 34226892 - 7th era - 0.03125", function(done) {
         var coin = BitEtherCoin.deployed();
-        coin.getEraForBlock.call(34226
+        coin.getEraForBlock.call(34226892).then( (e) => {
+  
