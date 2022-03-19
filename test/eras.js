62e0641b20be2004659b6a8589b57dca8b63d684
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2022-03-19 15:47:39 +0000

    drm/nouveau/gem: return only valid domain when there's only one

--- test/eras.js
+++ test/eras.js
@@ -218,4 +218,5 @@ contract('Calculate Era', function(accounts) {
             assert.equal(e[1].toNumber(), 39476892);
             //                            .00000000
             assert.equal(e[2].toNumber(),   1562500);
-            assert.equal(e[3].toNumber
+            assert.equal(e[3].toNumber(),   3125000);
+   
