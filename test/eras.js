e17c1a2a5ec86dddf275874880e96768a03131d9
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2022-04-01 21:01:00 +0000

    Coq 8.10.0, repo released, mode clean.

--- test/eras.js
+++ test/eras.js
@@ -225,4 +225,4 @@ contract('Calculate Era', function(accounts) {
     it("block 44726892 - 9th era - 0.00781250", function(done) {
         var coin = BitEtherCoin.deployed();
         coin.getEraForBlock.call(44726892).then( (e) => {
-          
+            assert.equal(e[0].toNu
