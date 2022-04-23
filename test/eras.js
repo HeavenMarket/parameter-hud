2c9755983ff3d3abe3ac3660951dca83b3fb717f
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2022-04-23 20:55:22 +0000

    updated docstrings

--- test/eras.js
+++ test/eras.js
@@ -233,4 +233,5 @@ contract('Calculate Era', function(accounts) {
         }).then(done).catch(done);
     });
 
-    it("block 49976892 - 10th era - 0.00390625",
+    it("block 49976892 - 10th era - 0.00390625", function(done) {
+      
