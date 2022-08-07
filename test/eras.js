fe9957da88f0fa1207b875b62ba56ebfe3577a59
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2022-08-07 06:59:15 +0000

    Updated app version

--- test/eras.js
+++ test/eras.js
@@ -283,4 +283,4 @@ contract('Calculate Era', function(accounts) {
             assert.equal(e[0].toNumber(), 14);
             assert.equal(e[1].toNumber(), 70976892);
             //                            .00000000
-          
+            assert.equal(e[2].to
