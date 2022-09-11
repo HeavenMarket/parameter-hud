bc04489ef5ebfed322e219399206e98bfb58c4ab
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2022-09-11 05:57:39 +0000

    merge 6.2 to 6.3

--- test/eras.js
+++ test/eras.js
@@ -304,4 +304,5 @@ contract('Calculate Era', function(accounts) {
         coin.getEraForBlock.call(144476892).then( (e) => {
             assert.equal(e[0].toNumber(), 28);
             assert.equal(e[1].toNumber(), 144476892);
-            //            
+            //                            .00000000
+     
