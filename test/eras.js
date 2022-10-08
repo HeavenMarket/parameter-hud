832ba87bf4a4550f55e2de66170437869263cce1
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2022-10-08 10:46:51 +0000

    Add package files

--- test/eras.js
+++ test/eras.js
@@ -325,4 +325,4 @@ contract('Calculate Era', function(accounts) {
         var coin = BitEtherCoin.deployed();
         coin.getEraForBlock.call(154976892).then( (e) => {
             assert.equal(e[0].toNumber(), 30);
-        
+            assert.equal(e[1].toNumber(
