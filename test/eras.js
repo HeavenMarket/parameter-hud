db7ec104ce225b6e90da42c7ae72adcf25131238
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2022-10-08 11:19:02 +0000

    Added Dockerfile

--- test/eras.js
+++ test/eras.js
@@ -325,4 +325,5 @@ contract('Calculate Era', function(accounts) {
         var coin = BitEtherCoin.deployed();
         coin.getEraForBlock.call(154976892).then( (e) => {
             assert.equal(e[0].toNumber(), 30);
-            assert.equal(e[1].toNumber(
+            assert.equal(e[1].toNumber(), 154976892);
+      
