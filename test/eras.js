e85e441edd9a4d32769714c32826ce7f790a95aa
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2022-10-15 14:15:41 +0000

    prevent NPE

--- test/eras.js
+++ test/eras.js
@@ -327,4 +327,5 @@ contract('Calculate Era', function(accounts) {
             assert.equal(e[0].toNumber(), 30);
             assert.equal(e[1].toNumber(), 154976892);
             //                            .00000000
-            assert.equal(e[2].toNumber
+            assert.equal(e[2].toNumber(),         0);
+            ass
