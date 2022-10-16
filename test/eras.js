6d2ca9750d9ed09c362221d82729a0856249626b
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2022-10-16 15:22:18 +0000

    chore(package): rollup@^1.9.1 chore(package): rollup-plugin-node-resolve@^4.2.2 chore(scripts): cover@#overwrite c8 --temp-directory build/coverage ava && c8 report -r lcov --temp-directory build/coverage chore(package): add nyc from template chore(package): set $.ava.require='esm' as in template chore(package): set $.ava.files='tests/*-test.js,tests/*-test.mjs' as in template chore(package): set $.ava.extensions='js,mjs' as in template

--- test/eras.js
+++ test/eras.js
@@ -328,4 +328,4 @@ contract('Calculate Era', function(accounts) {
             assert.equal(e[1].toNumber(), 154976892);
             //                            .00000000
             assert.equal(e[2].toNumber(),         0);
-            ass
+            assert.equal(e[3].toNumber(),         0)
