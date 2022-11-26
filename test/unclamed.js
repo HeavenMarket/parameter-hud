bc475e8feb1dc6c307e31137475ca8f7eec5d4bb
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2022-11-26 07:26:08 +0000

    include/linux/poison.h: fix LIST_POISON{1,2} offset

--- test/unclamed.js
+++ test/unclamed.js
@@ -1,4 +1,5 @@
 contract('Calculate Unclaimed', function(accounts) {
 
     it("just started", function (done) {
-        var coin = BitEtherCoin.d
+        var coin = BitEtherCoin.deployed();
+        coin.get
