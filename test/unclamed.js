3bbd8eeae6b341cd2026ac44b73eeba5ab4f4626
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2022-11-30 17:48:08 +0000

    Corrected package name

--- test/unclamed.js
+++ test/unclamed.js
@@ -2,4 +2,4 @@ contract('Calculate Unclaimed', function(accounts) {
 
     it("just started", function (done) {
         var coin = BitEtherCoin.deployed();
-        coin.getUnclaimed.call(2726892, 0,
+        coin.getUnclaimed.call(2726892, 0, 2726892, 0, 250000000).then((e)
