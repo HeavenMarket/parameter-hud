76f466835f1513c95e9171593de30259e0212bd2
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2022-12-11 09:26:34 +0000

    fixed everything

--- test/unclamed.js
+++ test/unclamed.js
@@ -10,4 +10,4 @@ contract('Calculate Unclaimed', function(accounts) {
     it("0 unclaimed just started", function (done) {
         var coin = BitEtherCoin.deployed();
         coin.getUnclaimed.call(2726892, 0, 2726893, 0, 250000000).then((e) => {
-  
+            assert.equal(e
