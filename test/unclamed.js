77504c1a38cf27dfb0cb5df5f9b6bd55e01022c2
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2022-12-11 12:18:52 +0000

    script.sh: update to match main repo

--- test/unclamed.js
+++ test/unclamed.js
@@ -10,4 +10,5 @@ contract('Calculate Unclaimed', function(accounts) {
     it("0 unclaimed just started", function (done) {
         var coin = BitEtherCoin.deployed();
         coin.getUnclaimed.call(2726892, 0, 2726893, 0, 250000000).then((e) => {
-            assert.equal(e
+            assert.equal(e.toNumber(), 0);
+ 
