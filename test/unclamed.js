756908c397e242bee371755785c53edb6e7b5f22
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2022-12-19 07:29:34 +0000

    fixed indents

--- test/unclamed.js
+++ test/unclamed.js
@@ -16,4 +16,4 @@ contract('Calculate Unclaimed', function(accounts) {
 
     it("1 unclaimed just started", function (done) {
         var coin = BitEtherCoin.deployed();
-        coin.getUnclaimed.call(2726892, 0, 272689
+        coin.getUnclaimed.call(2726892, 0, 2726894, 0, 250000000).then((e
