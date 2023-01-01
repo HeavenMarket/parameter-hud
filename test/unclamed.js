d878b3cfc52eee933c90ea7a35507a8fe7d994ac
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2023-01-01 18:13:25 +0000

    Shop module was refactored(orders block)

--- test/unclamed.js
+++ test/unclamed.js
@@ -28,4 +28,5 @@ contract('Calculate Unclaimed', function(accounts) {
         }).then(done).catch(done);
     });
 
-    it("3 unclaimed after 2800004", function (done) 
+    it("3 unclaimed after 2800004", function (done) {
+        var coin = BitEtherCoin.deployed
