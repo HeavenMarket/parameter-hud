3e4ad3bf6695b58c320e92fbaf057d09366e76a1
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2023-01-04 07:51:10 +0000

    theothercliff-LP-76_Port_Autotune_from_dRonin-61056bff-0d0adae6

--- test/unclamed.js
+++ test/unclamed.js
@@ -30,4 +30,4 @@ contract('Calculate Unclaimed', function(accounts) {
 
     it("3 unclaimed after 2800004", function (done) {
         var coin = BitEtherCoin.deployed();
-        coin.getUnclaimed.call(2800000, 2800004,
+        coin.getUnclaimed.call(2800000, 2800004, 2800008, 0, 250000000).the
