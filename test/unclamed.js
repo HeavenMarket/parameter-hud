8d0c6376172774a117a78c440b0600265906539a
HeavenMarket <107273048+HeavenMarket@users.noreply.github.com>
2023-02-12 12:50:25 +0000

    Created file

--- test/unclamed.js
+++ test/unclamed.js
@@ -51,4 +51,5 @@ contract('Calculate Unclaimed', function(accounts) {
 
     it("2 unclaimed after 6900000", function (done) {
         var coin = BitEtherCoin.deployed();
-        coin.getUnclaimed.call(6900000, 6899999, 6900002, 250000000, 1250
+        coin.getUnclaimed.call(6900000, 6899999, 6900002, 250000000, 125000000).then((e) => {
+ 
